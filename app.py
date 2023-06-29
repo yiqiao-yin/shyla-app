649c2d26450638b550a2e2feaa34c4b6abdc33cf
Yiqiao Yin <32333117+yiqiao-yin@users.noreply.github.com>
2023-06-28 23:30:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 
+# Here: TODO
+# TO have palm function loaded
+# you'll need API key
+# palm_api_key = st.secrets["PALM_API_KEY"]
+# you'll have to define the function
+# def call_palm()
+# HERE
+# import google.generativeai as palm
+# def call_palm(prompt: str, palm_api_key: str) -> str:
+#     palm.configure(api_key=palm_api_key)
+#     completion = palm.generate_text(
+#         model="models/text-bison-001",
+#         prompt=prompt,
+#         temperature=0,
+#         max_output_tokens=800,
+#     )
+#     return completion.result
+
 st.title("Echo Bot")
 
 # Initialize chat history
@@ -18,6 +36,12 @@ if prompt := st.chat_input("What is up?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
+    # TODO
+    # You have to update the following line of code
+    # to add the function that you want. Say you want to add 
+    # call_palm() function. Then you need to define in the script
+    # and call it here. 
+    # You'll have response = call_palm(f"{prompt}")
     response = f"Echo: {prompt}"
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
